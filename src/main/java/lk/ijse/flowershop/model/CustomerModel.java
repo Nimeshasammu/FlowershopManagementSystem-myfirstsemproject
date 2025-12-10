8785b4285e8e553b599da7dec0609c4f46872fb7
Nimeshasammu <Sammunimesha2@gmail.com>
2025-12-10 22:31:08 +0530

    update crudutill

--- src/main/java/lk/ijse/flowershop/model/CustomerModel.java
+++ src/main/java/lk/ijse/flowershop/model/CustomerModel.java
@@ -1,23 +1,23 @@
-//package lk.ijse.flowershop.model;
-//
-//import lk.ijse.flowershop.dto.CustomerDto;
-//
-//import java.util.ArrayList;
-//
-//public class CustomerModel {
-//    public boolean customerSave(CustomerDto customerDto) {
-//
-//    }
-//
-//
-//    public boolean customerUpdate(CustomerDto customerDto) {
-//
-//    }
-//
-//    public boolean customerDelete(CustomerID customerID){
-//
-//    }
-//    public ArrayList<CustomerDto>getAllCustomer(){
-//
-//    }
-//}
+package lk.ijse.flowershop.model;
+
+import lk.ijse.flowershop.dto.CustomerDto;
+
+import java.util.ArrayList;
+
+public class CustomerModel {
+    public boolean customerSave(CustomerDto customerDto) {
+
+    }
+
+
+    public boolean customerUpdate(CustomerDto customerDto) {
+
+    }
+
+    public boolean customerDelete(CustomerID customerID){
+
+    }
+    public ArrayList<CustomerDto>getAllCustomer(){
+
+    }
+}
